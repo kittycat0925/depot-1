9a047d23e80e6bb45119a4609cf04957f52156ec
Tim <tim@clusterfork.com>
2012-05-01 19:54:58 -0500

    Fixed create function to redirect properly, fixed test to check for proper variables, removed mass_assign protection from order.rb

--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -28,7 +28,7 @@ class OrdersControllerTest < ActionController::TestCase
 
   test "should create order" do
     assert_difference('Order.count') do
-      post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
+      post :create, order: @order.attributes
     end
 
     assert_redirected_to store_path
