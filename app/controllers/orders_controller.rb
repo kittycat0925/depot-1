5317f0896dcd8c536c6bab1c74473b6c80abb44a
Tim <tim@clusterfork.com>
2012-05-12 18:23:13 -0500

    Updating render

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -61,7 +61,7 @@ class OrdersController < ApplicationController
         format.json { render json: @order, status: :created, location: @order }
       else
         @cart = current_cart
-        format.html { render action: "new" }
+        format.html { render "new" }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
